0186d6f0946f5778a1ee9359345941135eca67bc
Cj Alexander <clauvinsalexander@gmail.com>
2024-10-26 12:42:05 -0400

    set up api

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-// entry point of api
+// use process.env to access variables in .env in the root of backend directory.
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+// necessary middleware in order to receive requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routing configuration
+const imageRouter = require('./routes/imageRoutes')
+app.use('/api/image')
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`server listening on port ${port}`))
