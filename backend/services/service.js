7cc22d0f2fe7ab4b14477ed0c5503aa38a14c300
Gabriel Burger <GEBurger0224@gmail.com>
2024-10-26 18:39:57 -0400

    Merge branch api-development

--- backend/services/service.js
+++ backend/services/service.js
@@ -1,1 +1,33 @@
-// service in this dir
+require('dotenv').config()
+const { WatsonXAI } = require('@ibm-cloud/watsonx-ai');
+
+
+process.env.IBM_CREDENTIALS_FILE = './Watson.env'; 
+
+const watsonxAIService = WatsonXAI.newInstance({
+    version: '2024-10-26',
+    serviceUrl: 'https://us-south.ml.cloud.ibm.com',
+  });
+
+const textGenRequestParametersModel = {
+    max_new_tokens: 100,
+};
+
+const params = {
+    input: "What is the capital of Thailand? ",
+    modelId: 'ibm/granite-13b-chat-v2',
+    projectId: '1ca4bf2e-97f4-4fa7-b6e7-f3c688fde604',
+    parameters: textGenRequestParametersModel,
+};
+
+const textGeneration = async () => {
+    console.log('hey')
+    // const output = await watsonxAIService.generateText(params)
+    // console.log(output)
+}
+
+function test() {
+    console.log('h')
+}
+test()
+textGeneration();
